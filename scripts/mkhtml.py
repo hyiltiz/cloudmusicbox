8888d89ebab370236a18294e0e7534b3e79eb8a7
Hörmetjan Yiltiz <hyiltiz@gmail.com>
2013-05-03 00:23:36 +0800

    Add up the Licensing headers and file

--- scripts/mkhtml.py
+++ scripts/mkhtml.py
@@ -1,9 +1,33 @@
+# Copyright (C) <2013>  <Yeshiwei>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 #coding=utf-8
 import os, eyeD3,uuid
 sysroots=['/home/psy/music']
 searchpath=['/home/psy/music/']
 webroot='http://notes.yeshiwei.com/psy/music'
 default_poster='http://notes.yeshiwei.com/Music/defaultposter.jpg'
+
+LICENSE_TERM = """
+<cloudmusicbox>  Copyright (C) <2013>  <Yeshiwei>'
+This program comes with ABSOLUTELY NO WARRANTY; for details see LICENSE'
+This is free software, and you are welcome to redistribute it'
+under certain conditions; see LICENSE for details.'
+"""
+print(LICENSE_TERM)
 
 def mystr(s):
     if type(s)==type(u'a'):
--- scripts/mp32ogg.py
+++ scripts/mp32ogg.py
@@ -1,4 +1,20 @@
 #! /usr/bin/python2
+
+# Copyright (C) <2013>  <Yeshiwei>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 import os
 
 
@@ -6,12 +22,12 @@ def checkpath(path):
     global Allsongs
     if os.path.isdir(path):
         os.chdir(path)
-        dirlist=os.listdir(path)
+        dirlist = os.listdir(path)
         for i in dirlist:
             checkpath(path+'/'+i)
 
     else:
-        if path[-4:]==".mp3" and not os.path.exists(path[:-4]+'.ogg'):
+        if path[-4:] == ".mp3" and not os.path.exists(path[:-4]+'.ogg'):
             os.system('mp32ogg '+path)
 
 
